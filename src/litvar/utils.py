c77d9bf831510237dd6a2f1adb867fde2f91dd20
sean <sean_delong@hotmail.ca>
2021-11-11 16:58:45 -0500

    modified util function; updated version

--- src/litvar/utils.py
+++ src/litvar/utils.py
@@ -1,9 +1,20 @@
 from . import core
+import copy
 
 
-def publications_from_query(query):
+def publications_from_query(query:str):
+	complete_var_dict = {}
 	var_models = core.variation_search(query)
+	for model in var_models:
+		complete_var_dict[model.rsid] = complete_var_dict.get(model.rsid, {"hgvs": None, "pmids": set()})
+		complete_var_dict[model.rsid]["hgvs"] = model.hgvs
+
 	var_rsids = map(lambda x: x.rsid, var_models)
-	pmids = core.pmids_by_rsids(list(var_rsids))
-	return pmids
+	pmid_models = core.pmids_by_rsids(list(var_rsids))
+
+	for pmid_list in pmid_models:
+		for pmid in pmid_list.pmids:
+			complete_var_dict[pmid_list.rsid]["pmids"].add(pmid)
+
+	return complete_var_dict
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,4 +5,4 @@ from src.litvar import core
 class TestUtils:
 	def test_publications_from_query(self):
 		var_pmids = utils.publications_from_query("VHL")
-		assert all([isinstance(var_pmid, core.Model) for var_pmid in var_pmids])
+		assert all([key.startswith("rs") for key, val in var_pmids.items()])
